c577a5c00e9fe45ca0e9fa326b8c93a333f44cb4
shadowshot-x <ujjwal26599@gmail.com>
2023-01-25 13:41:31 +0530

    flex template creation added

--- main.tf
+++ main.tf
@@ -12,6 +12,14 @@ provider "google-beta" {
   zone    = var.zone
 }
 
+resource "null_resource" "flex_template" {
+
+ provisioner "local-exec" {
+    
+    command = "gcloud dataflow flex-template build gs://test-tf-flex/samples/dataflow/templates/streaming-beam-sql.json       --image-gcr-path \"us-central1-docker.pkg.dev/cft-test-375007/test-tf-flex-repo/dataflow/streaming-beam-sql:latest\"       --sdk-language \"JAVA\"       --flex-template-base-image JAVA11       --metadata-file \"./streaming_beam_sql/metadata.json\"       --jar \"./streaming_beam_sql/target/streaming-beam-sql-1.0.jar\"       --env FLEX_TEMPLATE_JAVA_MAIN_CLASS=\"org.apache.beam.samples.StreamingBeamSql\""
+  }
+}
+
 resource "google_dataflow_flex_template_job" "big_data_job" {
   count=var.df_flex_count
   provider                = google-beta
